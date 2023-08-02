fc419249073cc5715779ec0a58347c42f6fc74f7
NguyenQuocKhanh0 <khanh200175@gmail.com>
2023-08-02 15:36:47 +0700

    up

--- hparams.py
+++ hparams.py
@@ -19,15 +19,15 @@ def create_hparams(hparams_string=None, verbose=False):
         dist_url="tcp://localhost:54321",
         cudnn_enabled=True,
         cudnn_benchmark=False,
-        ignore_layers=['embedding.weight'],
+        ignore_layers=[''],
 
         ################################
         # Data Parameters             #
         ################################
         load_mel_from_disk=False,
-        training_files='filelists/ljs_audio_text_train_filelist.txt',
-        validation_files='filelists/ljs_audio_text_val_filelist.txt',
-        text_cleaners=['english_cleaners'],
+        training_files='/kaggle/input/traintxt/train.txt',
+        validation_files='/kaggle/input/testtxt/test.txt',
+        text_cleaners=['basic_cleaners'],
 
         ################################
         # Audio Parameters             #
